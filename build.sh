19e753bce22451418f42197bff196868c0e89d4f
TanPuc <htanphuc1212@gmail.com>
2024-05-07 11:25:15 +0700

    a

---
+++ build.sh
@@ -0,0 +1,6 @@
+
+cp \
+    node_modules/@ricky0123/vad-web/dist/silero_vad.onnx \
+    node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js \
+    node_modules/onnxruntime-web/dist/*.wasm \
+    dist
