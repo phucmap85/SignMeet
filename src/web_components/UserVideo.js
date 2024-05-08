5d3e55c2f91d94e0931a1731a843bfba460eb13d
Le Quang Phuc <phuclequang2006@gmail.com>
2024-05-08 11:06:59 +0700

    Add enter button

--- src/web_components/UserVideo.js
+++ src/web_components/UserVideo.js
@@ -10,6 +10,16 @@ function getTime() {
     return Date.now() / 1000;
 }
 
+function hamming_dist(x, y, u, v) {
+    return Math.sqrt((x - u) * (x - u) + (y - v) * (y - v));
+}
+
+function landmark_to_pixel(x, y) {
+    let new_x = Math.floor(x * 1280)
+    let new_y = Math.floor(y * 720)
+    return ({new_x, new_y})
+}
+
 let lm_arr = [];
 
 class UserVideo extends Component {
@@ -27,6 +37,11 @@ class UserVideo extends Component {
         this.on_lip = 0;
         this.on_passed = 0;
         this.pre_num_of_frames = 0;
+
+        this.t0_hand = getTime();
+        this.on_countdown_hand = 0;
+        this.on_enter = 0;
+        this.on_passed_hand = 0;
 
         lm_arr = [];
     }
@@ -100,9 +115,9 @@ class UserVideo extends Component {
         let lip_x_min = face[61][0], lip_x_max = face[409][0];
         let lip_y_min = Math.min(face[37][1], face[267][1]), lip_y_max = face[17][1]
 
-        // console.log(lip_x_min, lip_x_max, lip_y_min, lip_y_max)
-        console.log(getTime() - this.t0, this.on_lip);
+        // console.log(getTime() - this.t0, this.on_lip);
 
+        // Check if right hand on lip
         if (rh[12][0] > 0 && rh[12][1] > 0 && rh[12][0] >= lip_x_min && rh[12][0] <= lip_x_max
             && rh[12][1] >= lip_y_min && rh[12][1] <= lip_y_max) {
 
@@ -132,12 +147,34 @@ class UserVideo extends Component {
 
                 this.sendHTTPReq(lm_arr);
 
-                // runModel(lm_arr).then(res => {
-                //     console.log("Prediction: ", indexOfMax(res));
-                // });
-
                 (lm_arr = []).length = 0;
             }
+        }
+
+        // Check if user click enter
+        let new_x_hand = landmark_to_pixel(rh[12][0], rh[12][1]).new_x;
+        let new_y_hand = landmark_to_pixel(rh[12][0], rh[12][1]).new_y;
+
+        console.dir(hamming_dist(new_x_hand, new_y_hand, 50, 400));
+        if (hamming_dist(new_x_hand, new_y_hand, 50, 400) <= 30) {
+            if (!this.on_countdown_hand) {
+                this.t0_hand = getTime();
+                this.on_countdown_hand = 1;
+            }
+
+            if (2.0 - getTime() + this.t0_hand <= 0 && !this.on_passed_hand) {
+                this.on_passed_hand = 1;
+                this.on_enter = 1 - this.on_enter;
+            }
+        } else {
+            this.on_passed_hand = 0;
+            this.on_countdown_hand = 0;
+        }
+
+        if (this.on_enter) {
+            // Send request here
+            console.log('da gui');
+            this.on_enter = 0;
         }
 
         // Set canvas width
@@ -156,6 +193,30 @@ class UserVideo extends Component {
             canvasElement.width,
             canvasElement.height
         );
+
+        var centerX = 25;
+        var centerY = 267;
+        var radius = 15;
+
+        canvasCtx.beginPath();
+        canvasCtx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        canvasCtx.fillStyle = 'lightblue';
+        canvasCtx.fill();
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = 'black';
+        canvasCtx.stroke();
+
+        // Draw arrow
+        var arrowLength = radius * 1.3;
+        var arrowWidth = radius * 0.5;
+
+        canvasCtx.beginPath();
+        canvasCtx.moveTo(centerX - radius * 0.5, centerY);
+        canvasCtx.lineTo(centerX - radius + arrowLength, centerY - arrowWidth);
+        canvasCtx.lineTo(centerX - radius + arrowLength, centerY + arrowWidth);
+        canvasCtx.closePath();
+        canvasCtx.fillStyle = 'black';
+        canvasCtx.fill();
 
 
         drawConnectors(canvasCtx, results.poseLandmarks, hlt.POSE_CONNECTIONS,
